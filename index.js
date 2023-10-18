033db221e7ab993742a845dd778e9c2b16a42ea1
Cazpar <casti19@student.sdu.dk>
2023-10-17 22:13:32 -0700

    feat: :sparkles: Reset grid button

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-// add a 16x16 grid of square divs to div.game-container
-
-const gameContainer = document.getElementsByClassName("game-container");
+const gameContainer = document.getElementsByClassName("game-grid");
+const gameSize = 16;
 
 function createGrid() {
-    for (let i = 0; i < 255; i++) {
+    for (let i = 0; i < 256; i++) {
         let square = document.createElement("div");
-        square.classList.add("square");
+        square.classList.add("grid-element");
         gameContainer[0].appendChild(square);
     }
 }
 createGrid();
+
+
+// script to leave a hover trail
+const squares = document.querySelectorAll(".grid-element");
+
+squares.forEach(square => {
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black";
+    });
+});
+
+// script to clear the grid
+const clearButton = document.querySelector(".reset-button");
+
+clearButton.addEventListener("click", () => {
+    squares.forEach(square => {
+        square.style.removeProperty("background-color");
+    });
+}
+);
