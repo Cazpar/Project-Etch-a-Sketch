62a3897656ece321925e0196e9b6e0365844723a
Cazpar <casti19@student.sdu.dk>
2023-10-18 14:27:26 -0700

    feat: :sparkles: Add input for gameSize and update grid and label
    The user can now choose the size of their grid, the current grid is cleared and a new one with chosen size is created

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 import { Grid } from './grid.js';
 
-const gameSize = 16;
+// create grid
+let gameSize = 16;
 const myGrid = new Grid(gameSize);
 myGrid.createGrid();
 
+const label = document.querySelector('.game-size-label');
+label.textContent = `Grid Size: ${myGrid.size}`;
+
+
+// clear grid
 const clearButton = document.querySelector('.reset-button');
 clearButton.addEventListener('click', () => {
     myGrid.updateGridSize(gameSize);
+});
+
+// get slider value from #gameSize and update grid
+const slider = document.querySelector('#gameSize');
+
+slider.addEventListener('change', () => {
+    gameSize = slider.value;
+    myGrid.updateGridSize(gameSize);
+
+    // update label
+    label.textContent = `Grid Size: ${myGrid.size}`;
 });
