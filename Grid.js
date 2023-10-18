e1075a408e98506ffd0f07d9a4ec2059094f7641
Cazpar <casti19@student.sdu.dk>
2023-10-18 14:12:35 -0700

    refactor: :recycle: Move grid into its own class
    Encapsulate the grid into its own class

---
+++ Grid.js
@@ -0,0 +1,63 @@
+//create class game for all game logic
+class Grid {
+    // constructor for game
+    constructor(size) {
+        this.size = size;
+        this.container = document.querySelector('.game-container');
+        this.gridWidth = this.container.clientWidth;
+        this.gridHeight = this.container.clientHeight;
+    }
+
+    // construct grid based on size into div.game-grid
+    createGrid() {
+        const gameGrid = document.querySelector('.game-grid');
+        const gridWidth = gameGrid.clientWidth;
+        const gridHeight = gameGrid.clientHeight;
+
+        // construct rows based on size given and add them to gameGrid
+        for (let i = 0; i < this.size; i++) {
+            const row = document.createElement('div');
+            row.classList.add('row');
+            row.style.height = `${gridHeight / this.size}px`;
+            row.style.boxSizing = 'border-box';
+
+            // construct cells based on size given and add them to row
+            for (let j = 0; j < this.size; j++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.style.width = `${gridWidth / this.size}px`;
+                cell.style.height = `${gridHeight / this.size}px`;
+                row.appendChild(cell);
+            }
+            gameGrid.appendChild(row);
+        }
+        this.hoverCell();
+    }
+
+    // clear grid
+    clearGrid() {
+        const gameGrid = document.querySelector('.game-grid');
+        while (gameGrid.firstChild) {
+            gameGrid.removeChild(gameGrid.firstChild);
+        }
+    }
+
+    // update grid
+    updateGridSize(size) {
+        this.size = size;
+        this.clearGrid();
+        this.createGrid();
+    }
+
+    //callable function to change color on hover
+    hoverCell() {
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach(cell => {
+            cell.addEventListener('mouseover', (e) => {
+                e.target.style.backgroundColor = 'black';
+            });
+        });
+    }
+}
+
+export { Grid };
--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-const gameContainer = document.getElementsByClassName("game-grid");
+import { Grid } from './grid.js';
+
 const gameSize = 16;
+const myGrid = new Grid(gameSize);
+myGrid.createGrid();
 
-function createGrid() {
-    for (let i = 0; i < 256; i++) {
-        let square = document.createElement("div");
-        square.classList.add("grid-element");
-        gameContainer[0].appendChild(square);
-    }
-}
-createGrid();
-
-
-// script to leave a hover trail
-const squares = document.querySelectorAll(".grid-element");
-
-squares.forEach(square => {
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "black";
-    });
+const clearButton = document.querySelector('.reset-button');
+clearButton.addEventListener('click', () => {
+    myGrid.updateGridSize(gameSize);
 });
-
-// script to clear the grid
-const clearButton = document.querySelector(".reset-button");
-
-clearButton.addEventListener("click", () => {
-    squares.forEach(square => {
-        square.style.removeProperty("background-color");
-    });
-}
-);
